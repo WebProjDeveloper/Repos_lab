2fec8b30dc2ee8e6c8edd7519aad0bf302f01440
WebDeveloper <aghzerwassima123@gmail.com>
2023-11-16 19:33:13 +0100

    lab2

---
+++ Lab2/Lab_23_AghzerHousna_a2.py
@@ -0,0 +1,14 @@
+# Deploy the model with streamlit
+# Amelioration comme la publicité du deployement sur streamlit website
+import streamlit as st
+import yfinance as yf
+
+st.header('Stock price prediction')
+st.image('datasets/bot.jpeg')
+symbol = 'AAPL'
+symbol = st.sidebar.selectbox('Select your stock symbol', ['AAPL', 'GOOGL', 'MSFT', 'TSLA', 'IAM', 'MSA'])
+st.write(symbol)
+start_date = st.sidebar.date_input('Start date')
+end_date = st.sidebar.date_input('End date')
+dataset_train = yf.download(symbol, start=start_date, end=end_date)
+st.write(dataset_train)
