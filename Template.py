4d1712a3e541360e5f38b9603cd2e3383c5a85d4
WebDeveloper <aghzerwassima123@gmail.com>
2023-11-16 19:38:25 +0100

    Labs

---
+++ Template.py
@@ -0,0 +1,8 @@
+# Lab21 : Prédiction de crédit logement
+# Réalisé par : Housna Aghzer /Emsi 2023-2024
+# Réference : https://www.kaggle.com/code/rodsonzepekinio/pr-vision-d-un-cr-dit-logement
+
+# Step 1 : DataSet
+# Step 2 : Model
+# Step 3 : Train
+# Step 4 : Test
